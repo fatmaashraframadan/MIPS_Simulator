690bc4182240c6e80eb1785e0bc005c541d894a9
mina faried <36136028+minafaried@users.noreply.github.com>
2019-12-12 19:01:54 +0200

    i did the machine code for add, addi and  sw
    i did the machine code for add, addi and  sw

--- Assembler.java
+++ Assembler.java
@@ -206,10 +206,114 @@ public class Assembler {
 
     }
 
+    static String get_register_number(String parameter) {
+        if(parameter.equals("$at")) {
+            return "00001";
+        }
+        else if(parameter.equals("$v0")) {
+            return "00010";
+        }
+        else if(parameter.equals("$v1")) {
+            return "00011";
+        }
+        else if(parameter.equals("$a0")) {
+            return "00100";
+        }
+        else if(parameter.equals("$a1")) {
+            return "00101";
+        }
+        else if(parameter.equals("$a2")) {
+            return "00110";
+        }
+        else if(parameter.equals("$a3")) {
+            return "00111";
+        }
+        else if(parameter.equals("$t0")) {
+            return "01000";
+        }
+        else if(parameter.equals("$t1")) {
+            return "01001";
+        }
+        else if(parameter.equals("$t2")) {
+            return "01010";
+        }
+        else if(parameter.equals("$t3")) {
+            return "01011";
+        }
+        else if(parameter.equals("$t4")) {
+            return "01100";
+        }
+        else if(parameter.equals("$t5")) {
+            return "01101";
+        }
+        else if(parameter.equals("$t6")) {
+            return "01110";
+        }
+        else if(parameter.equals("$t7")) {
+            return "01111";
+        }
+        else if(parameter.equals("$s0")) {
+            return "10000";
+        }
+        else if(parameter.equals("$s1")) {
+            return "10001";
+        }
+
+        else if(parameter.equals("$s2")) {
+            return "10010";
+        }
+        else if(parameter.equals("$s3")) {
+            return "10011";
+        }
+        else if(parameter.equals("$s4")) {
+            return "10100";
+        }
+        else if(parameter.equals("$s5")) {
+            return "10101";
+        }
+        else if(parameter.equals("$s6")) {
+            return "10110";
+        }
+
+        else if(parameter.equals("$s7")) {
+            return "10111";
+        }
+        else if(parameter.equals("$t8")) {
+            return "11000";
+        }
+        else if(parameter.equals("$t9")) {
+            return "11001";
+        }
+
+        else if(parameter.equals("$k0")) {
+            return "11010";
+        }
+        else if(parameter.equals("$k1")) {
+            return "11011";
+        }
+
+        else if(parameter.equals("$gp")) {
+            return "11100";
+        }
+        else if(parameter.equals("$sp")) {
+            return "11101";
+        }
+        else if(parameter.equals("$fp")) {
+            return "11110";
+        }
+        else if(parameter.equals("$ra")) {
+            return "11111";
+        }
+        else {
+            return "00000";
+        }
+
+    }
     // add $t0 $s1 $0
     // add $t0 $0 $0
     public void add(String[] parameters)
     {
+     
         int r1=get_register(parameters[1]);
         int r2=get_register(parameters[2]);
         System.out.println(r1+"  "+r2);
@@ -226,11 +330,12 @@ public class Assembler {
 
         set_register(parameters[0], r1+r2);
 
-        System.out.println(get_register("Result : " + parameters[0]));
+        System.out.println("Result : " + get_register(parameters[0]));
 
-    }
+        String machine="";
+        machine="000000"+" "+get_register_number(parameters[0])+" "+get_register_number(parameters[1])+" "+get_register_number(parameters[2])+" "+"00000"+" "+"100000";
+        System.out.println("machine code is : " +machine);
 
-    public void sub(String[] parameters) {
     }
     // add $t0 $s1 5
     // add $t0 $0 3
@@ -240,8 +345,23 @@ public class Assembler {
          {//md const value get the first address in memory=100000
              r1=memory.memorydata[r1-memory_address]; //md const value get the first address in memory=100000
          }
-         
-         set_register(parameters[0], r1+Integer.parseInt( parameters[2]));
+         int constant=Integer.parseInt( parameters[2]);
+         set_register(parameters[0], r1+constant);
+         String c="";
+         for (int i = 0; i < 16; i++) {
+			if(constant%2==0) {
+				c="0"+c;
+			}
+			else
+			{
+				c="1"+c;
+			}
+			constant/=2;
+		}
+         String machine="";
+         machine="001000"+" "+get_register_number(parameters[1])+" "+get_register_number(parameters[0])+" "+c;
+         System.out.println("machine code is : " +machine);
+
 
     }
 
@@ -260,6 +380,22 @@ public class Assembler {
          System.out.println(r1+"  "+r2+"  "+len);
          memory.memorydata[r2]=r1;
          
+         int offist=Integer.parseInt(len);
+         String c="";
+         for (int i = 0; i < 16; i++) {
+			if(offist%2==0) {
+				c="0"+c;
+			}
+			else
+			{
+				c="1"+c;
+			}
+			offist/=2;
+		}
+         String machine="";
+         machine="101011"+" "+get_register_number(temp)+" "+get_register_number(parameters[0])+" "+c;
+         System.out.println("machine code is : " +machine);
+
     }
     public void and(String[] parameters) { //not finished yet
    	 int r1=get_register(parameters[1]);
