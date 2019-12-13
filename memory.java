279f6e927d4bba43a46d43ccfcf418602ff197f9
fatmaashraframadan <36447147+fatmaashraframadan@users.noreply.github.com>
2019-12-13 15:54:11 +0200

    Update memory.java

--- memory.java
+++ memory.java
@@ -1,3 +1,9 @@
-public class memory {
+//package assemblersim;
+
+import java.util.ArrayList;
+
+public class memory
+{
+    static ArrayList<String>memoertext = new ArrayList<String>();
     static int[] memorydata=new int[100000];
 }
