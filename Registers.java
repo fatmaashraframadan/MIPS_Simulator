07c110990389a192bf6444489d5891c4abd74d42
Mariam Makram <36172189+mariammakram@users.noreply.github.com>
2019-12-18 04:03:06 +0200

    Update Registers.java

--- Registers.java
+++ Registers.java
@@ -1,6 +1,19 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Registers {
+
+    // Using it to fill GUI Table with Register Names
+    public static final List<String> registerNames = Arrays.asList("$0", "$at", "$v0", "$v1", "$a0", "$a1", "$a2",
+                                                                    "$a3", "$t0", "$t1", "$t2", "$t3", "$t4", "$t5",
+                                                                    "$t6", "$t7", "$s0", "$s1", "$s2", "$s3",
+                                                                    "$s4", "$s5", "$s6", "$s7", "$t8", "$t9",
+                                                                    "$k0", "$k1", "$gp", "$sp", "$fp", "$ra");
+    // Using it to fill GUI Table with Register Values
+    public static List<Integer> values = Arrays.asList(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 
     final static public int r_$0 = 0;
     static public int r_$at;
@@ -34,5 +47,27 @@ public class Registers {
     static public int r_$sp;
     static public int r_$fp;
     static public int r_$ra;
+
+    private String name;
+    private int address;
+    private int value;
+
+
+    public Registers() {
+        name = "";
+        address = 0;
+        value = 0;
+    }
+
+    public Registers(String name, int address, int value) {
+        this.name = name;
+        this.address = address;
+        this.value = value;
+    }
+
+    public int searchReg(String nameR){
+        int index = registerNames.indexOf(nameR);
+        return index;
+    }
 
 }
